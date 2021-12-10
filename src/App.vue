3f97226d1277b0bab0f32a36a31828a331ec810b
abblorsch <lorentz.schmidt@abbindustrigymnasium.se>
2021-12-10 14:18:05 +0100

    lök

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,91 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+       
+                <template>
+          <v-row justify="center">
+            <v-dialog
+              v-model="dialog"
+              persistent
+              max-width="600px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                <v-icon>mdi-account</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">User Profile</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                      >
+                      
+                      </v-col>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+                     
+                      <v-col cols="12">
+                        <v-text-field
+                          label="Email*"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          label="Password*"
+                          type="password"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                      >
+                      
+                        <v-autocomplete
+                          :items="['netflix', 'via-play', 'C more', 'HBO', 'diesny+', 'Amazon-prime', ]"
+                          label="vad är din streamingtjänst?"
+                          multiple
+                        ></v-autocomplete>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                  <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="dialog = false"
+                  >
+                    stäng
+                  </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="dialog = false"
+                  >
+                    spara
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+        </template>
+
+        
       </div>
 
       <v-spacer></v-spacer>
@@ -46,15 +114,17 @@
 <script>
 import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
 
+export default {
+    data: () => ({
+      dialog: false,
+    }),  
+    
   components: {
     HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
-};
+
+
+  }
 </script>
