b2ae2b785b935491cd2ee572e0a954ed18b7f301
abblorsch <lorentz.schmidt@abbindustrigymnasium.se>
2021-12-10 14:18:22 +0100

    igen

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
+import Icon from '@mdi/js'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
+   Icon
+    },
   mutations: {
   },
   actions: {
